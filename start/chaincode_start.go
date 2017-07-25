d52f040d624cda10d3843127030aca9e2c212fcb
manuelco <manuelco@mx1.ibm.com>
2017-07-25 13:23:20 -0500

    Read modified

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -84,7 +84,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 }
 
 //Read function
-func (t*SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
 	var key, jsonResp string
 	var err error
 	if len(args) != 1 {
@@ -93,8 +93,8 @@ func (t*SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) (
 	key=args[0]
 	valAsbytes, err:=stub.GetState(key)
 	if err!=nil {
-		jsonResp ="{\"Error\":\"Failed to get for" + key + "\"}"
-			return nil, errors.New(jsonResp)
+		jsonResp ="{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
 	}
 	return valAsbytes, nil
 }
